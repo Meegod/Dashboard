a323f0bb20ad44242b968dc59e58ee36de1cdade
Godfrey <[godfreyfreezey@gmail.com]>
2021-03-05 04:55:23 +0100

    dashboard commit

---
+++ assets/js/dashboard.js
@@ -0,0 +1,19 @@
+@media(max-width:700){
+
+.main-nav ul.main-menu{
+	display:block;
+	position:absolute;
+	top:0;
+	left:0;
+	backgrounnd:#f2f2f2;
+	width:50%;
+	height:100%;
+	border-right:#ccc 1px solid;
+	transform:translateX(-20px);
+	opacity:0.9;
+}
+.main-nav ul.main-menu .show{
+	transform:translateX(-20px);
+}
+
+}
